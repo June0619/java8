b4582cce9bab8133a606487af36585622d99826c
[아이앤소프트]정지운 <jjw5050@hotmail.com>
2022-06-20 18:22:59 +0900

    wip: 람다식 강의 내용 추가 중

---
+++ src/main/java/me/java8/LambdaMain.java
@@ -0,0 +1,49 @@
+package me.java8;
+
+import java.util.function.BinaryOperator;
+import java.util.function.Consumer;
+import java.util.function.IntConsumer;
+import java.util.function.Supplier;
+
+public class LambdaMain {
+    public static void main(String[] args) {
+
+        //BODY 내용이 한줄이라면 생략 가능
+//        Supplier<Integer> get10 = () -> {
+//            return 10;
+//        };
+
+        Supplier<Integer> get10 = () -> 10;
+        BinaryOperator<Integer> summary = (a, b) -> a + b;
+
+    }
+
+    private void run() {
+
+        final int baseNumber = 10;
+
+        //로컬 클래스
+        class LocalClass {
+            void printBaseNumber() {
+                System.out.println(baseNumber);
+            }
+        }
+
+        //익명 클래스
+        Consumer<Integer> integerConsumer = new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) {
+                System.out.println(baseNumber);
+            }
+        };
+
+        //람다에서 로컬 변수를 참조하고 있는 상황
+        IntConsumer printInt = (i) -> {
+            System.out.println(i + baseNumber);
+        };
+
+
+
+        printInt.accept(10);
+    }
+}
