5433c530005b5d7b28e61ef82f840b96f2fee741
June0619 <jjw5050@hotmail.com>
2022-06-23 14:10:20 +0900

    lecture: 메소드 참조

---
+++ src/main/java/me/java8/MethodReferenceMain.java
@@ -0,0 +1,36 @@
+package me.java8;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.function.UnaryOperator;
+
+public class MethodReferenceMain {
+
+    public static void main(String[] args) {
+
+        //static method 참조
+        UnaryOperator<String> hi = Greeting::hi;
+
+        //instance method 참조
+        Greeting greeting = new Greeting();
+        UnaryOperator<String> hello = greeting::hello;
+
+        //constructor 참조
+        Supplier<Greeting> newGreeting = Greeting::new;
+        Greeting suppliedGreeting = newGreeting.get();
+
+        //함수형 인터페이스에 따라 다른 종류의 생성자를 참조할 수도 있다.
+        Function<String, Greeting> stringGreetingFunction =
+                Greeting::new;
+        Greeting gr = stringGreetingFunction.apply("greeting");
+        System.out.println(gr.getName());
+
+        //임의의 객체를 참조하는 것도 가능하다.
+        String[] names = {"jwjung", "brokenBot", "ironman"};
+        Arrays.sort(names, String::compareToIgnoreCase);
+
+        System.out.println(Arrays.toString(names));
+    }
+}
