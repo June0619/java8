3e05ba99fd0ef390efed987514511252b54dae95
June0619 <jjw5050@hotmail.com>
2022-06-23 13:03:49 +0900

    lecture: 람다 표현식

--- src/main/java/me/java8/LambdaMain.java
+++ src/main/java/me/java8/LambdaMain.java
@@ -16,6 +16,8 @@ public class LambdaMain {
         Supplier<Integer> get10 = () -> 10;
         BinaryOperator<Integer> summary = (a, b) -> a + b;
 
+        LambdaMain lambdaMain = new LambdaMain();
+        lambdaMain.run();
     }
 
     private void run() {
@@ -25,7 +27,8 @@ public class LambdaMain {
         //로컬 클래스
         class LocalClass {
             void printBaseNumber() {
-                System.out.println(baseNumber);
+                int baseNumber = 11;
+                System.out.println("LocalClass : " + baseNumber);
             }
         }
 
@@ -33,16 +36,17 @@ public class LambdaMain {
         Consumer<Integer> integerConsumer = new Consumer<Integer>() {
             @Override
             public void accept(Integer integer) {
-                System.out.println(baseNumber);
+                int baseNumber = 11;
+                System.out.println("AnonymousClass : " + baseNumber);
             }
         };
 
         //람다에서 로컬 변수를 참조하고 있는 상황
         IntConsumer printInt = (i) -> {
+            // ERR
+//            int baseNumber = 11;
             System.out.println(i + baseNumber);
         };
-
-
 
         printInt.accept(10);
     }
