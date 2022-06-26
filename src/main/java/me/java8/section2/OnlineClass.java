26374d41ce80dd95d4cfdbf58f282003983610f8
June0619 <jjw5050@hotmail.com>
2022-06-26 21:44:27 +0900

    wip: Optional 수업내용 추가

--- src/main/java/me/java8/section2/OnlineClass.java
+++ src/main/java/me/java8/section2/OnlineClass.java
@@ -7,6 +7,8 @@ public class OnlineClass {
 
     private boolean closed;
 
+    public Progress progress;
+
     public OnlineClass(Integer id, String title, boolean closed) {
         this.id = id;
         this.title = title;
